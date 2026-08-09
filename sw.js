dfdf150cc07d2070fcfc0aef47f8c8fae4735629
jv11001100-hub <jv11001100@gmail.com>
2026-08-10 06:52:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'callqueue-pwa-v4';
+const CACHE = 'callqueue-pwa-v5';
 const ASSETS = ['./','index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
